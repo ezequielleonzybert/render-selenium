1e40a59505182c0fd801521823d05e0f82a84925
ezequiel <ezequielleonzybert@gmail.com>
2024-03-27 17:51:51 -0300

    xplatform

--- globals.py
+++ globals.py
@@ -2,8 +2,13 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.wait import WebDriverWait
+import platform
 
-service = Service(executable_path='chromedriver.exe')
+OS = platform.system()
+if OS == 'Windows':
+    service = Service(executable_path='chromedriver.exe')
+elif OS == 'Linux':
+    service = Service(executable_path='chromedriverlinux')
 options = Options()
 options.add_argument('--headless')
 options.add_argument('--log-level=3')
