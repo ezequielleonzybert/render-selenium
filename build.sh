c541b12b9c37f3fc0b2fe015b49984b365c5a489
ezequiel <ezequielleonzybert@gmail.com>
2024-04-01 15:19:56 -0300

    cd

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ source ~/.bash_profile
 pip install -r requirements.txt
 
 mkdir -p /chrome
+cd /chrome
 wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 dpkg -x ./google-chrome-stable_current_amd64.deb /chrome
