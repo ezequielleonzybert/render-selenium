594fc87d558ee4a94f11c40d40adbf575b4ff9d9
ezequiel <ezequielleonzybert@gmail.com>
2024-04-01 15:17:29 -0300

    ionstall chrome

--- build.sh
+++ build.sh
@@ -3,4 +3,6 @@ source ~/.bash_profile
 
 pip install -r requirements.txt
 
-apt-get install google-chrome-stable -y
+mkdir -p /chrome
+wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+dpkg -x ./google-chrome-stable_current_amd64.deb /chrome
